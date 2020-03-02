20131fe62bfc31e23b389a27998bfef99a5d0e57
egor.kulikov <egor.kulikov@arealidea.ru>
2020-03-02 16:34:16 +0300

    create .bat file

---
+++ .bat
@@ -0,0 +1,6 @@
+@echo off
+echo "Перевожу на сайт для установки Node.js"
+start "" https://nodejs.org/dist/v12.16.1/node-v12.16.1-x64.msi
+
+echo "Перевожу на сайт для установки Docker"
+start "" https://download.docker.com/win/beta/InstallDocker.msi
